6688c074c42c3377ca420c5ea9f52060546c4bce
Sky Bai(白喜阳) <baixiyang@inspur.com>
2019-04-17 23:31:41 +0800

    router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,10 @@ import HomePage from '../views/HomePage/HomePage'
 import ProfilePage from '../views/ProfilePage/ProfilePage'
 import avatarUpload from '../views/ProfilePage/avatarUpload'
 
-
 var indexRoutes=[
   {path:'/Login',name:'LoginPage',component:LoginPage},
   {path:'/Profile',name:'ProfilePage',component:ProfilePage},
   {path:'/avatarUpload',name:'avatarUpload',component:avatarUpload},
   {path:'/',name:'HomePage',component:HomePage}
 ];
-
 export default indexRoutes;
